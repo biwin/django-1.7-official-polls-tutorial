6a15d758f686a55e8ed03c0eebc124757d49d4e9
biwin <biwin007@gmail.com>
2014-12-04 18:08:33 +0530

    adding arbitrary html class

--- polls/admin.py
+++ polls/admin.py
@@ -5,14 +5,14 @@ from polls.models import Question
 
 
 class QuestionAdmin(admin.ModelAdmin):
-
 	# fields = ['pub_date', 'question_text']
 	# # pub_date moved to the top of the list
 	# using fieldsets to make fields more readable.
 
 	fieldsets = [
 		(None, {'fields': ['question_text']}),
-		('Date information', {'fields': ['pub_date']}),
+		('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+		# adds the arbitrary html class collapse.
 	]
 
 # hooks models to the admin site.
