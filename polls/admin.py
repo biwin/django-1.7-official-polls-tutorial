a88a75b14ce38e2fcd5a6bffeab0767c8b6d8c6c
biwin <biwin007@gmail.com>
2014-12-04 18:00:17 +0530

    arrange to the field sets.

--- polls/admin.py
+++ polls/admin.py
@@ -5,7 +5,16 @@ from polls.models import Question
 
 
 class QuestionAdmin(admin.ModelAdmin):
-	fields = ['pub_date', 'question_text']
-	# pub_date moved to the top of the list
 
+	# fields = ['pub_date', 'question_text']
+	# # pub_date moved to the top of the list
+	# using fieldsets to make fields more readable.
+
+	fieldsets = [
+		(None, {'fields': ['question_text']}),
+		('Date information', {'fields': ['pub_date']}),
+	]
+
+# hooks models to the admin site.
+# admin.site.register(Question)
 admin.site.register(Question, QuestionAdmin)
