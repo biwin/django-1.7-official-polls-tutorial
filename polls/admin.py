9d6076c73e1dc33a72047d05d3bb9f1bc9a1b283
biwin <biwin007@gmail.com>
2014-12-06 23:10:14 +0530

    time ago display

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from polls.models import Question, Choice
+import datetime
 
 # Register your models here.
 
@@ -33,6 +34,14 @@ class QuestionAdmin(admin.ModelAdmin):
 		# adds the arbitrary html class collapse.
 	]
 	inlines = [ChoiceInline]
+
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+	
+	was_published_recently.admin_order_field = 'pub_date'
+	was_published_recently.boolean = True
+	was_published_recently.short_description = 'Published recently?'
+	list_filter = ['pub_date']
 
 # hooks models to the admin site.
 # admin.site.register(Question)
